f2f8daa0697adedbfa0d4b6640d18cdf819bcfb5
kyoung-yeon <randy43@naver.com>
2022-05-17 20:20:24 +0900

    BOJ_1000 / A+B / 사칙연산

---
+++ src/BOJ/BOJ_1000.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a+b);
+    }
+}

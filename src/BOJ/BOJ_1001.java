0b6e329c01936ebda5687bf622725ec1e3cef925
kyoung-yeon <randy43@naver.com>
2022-05-17 20:33:45 +0900

    BOJ_1001 / A-B / 사칙연산

---
+++ src/BOJ/BOJ_1001.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a-b);
+    }
+}

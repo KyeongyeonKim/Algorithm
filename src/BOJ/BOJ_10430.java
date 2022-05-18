be1f8a293cbccb0d87be8db8ff05e93fbe764c6b
kyoung-yeon <randy43@naver.com>
2022-05-18 17:16:11 +0900

    BOJ_10430 / 나머지 / 사칙연산

---
+++ src/BOJ/BOJ_10430.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_10430 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}

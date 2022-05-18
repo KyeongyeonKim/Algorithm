5c3a34ba9f5786192e0542cb2944f989c9dfcec4
kyoung-yeon <randy43@naver.com>
2022-05-18 22:03:05 +0900

    BOJ_3046 / R2 / 사칙연산

---
+++ src/BOJ/BOJ_3046.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_3046 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int R1 = input.nextInt();
+        int S = input.nextInt();
+
+        System.out.println((2*S)-R1);
+}
+}

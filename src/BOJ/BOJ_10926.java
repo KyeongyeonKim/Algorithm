8db28d13e6d0d196d01bc7aeb8171f9e54580154
kyoung-yeon <randy43@naver.com>
2022-05-18 20:17:19 +0900

    BOJ_10926 / ??! / 구현

---
+++ src/BOJ/BOJ_10926.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String id = input.next();
+
+        System.out.println(id+"??!");
+    }
+}

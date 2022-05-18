4c95071b4cef22b851b4d4fb2dadc0fe139acfea
kyoung-yeon <randy43@naver.com>
2022-05-18 20:26:09 +0900

    BOJ_18108 / 1998년생인 내가 태국에서는 2541년생?! / 사칙연산

---
+++ src/BOJ/BOJ_18108.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int y = input.nextInt();
+
+        System.out.println(y-543);
+    }
+
+}

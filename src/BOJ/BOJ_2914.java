6f4195653757409feb91258b8246b58f837eedb9
KY <rudyeon@naver.com>
2022-05-19 00:38:23 +0900

    BOJ_2914 / 저작권 / 사칙연산

--- src/BOJ/BOJ_2914.java
+++ src/BOJ/BOJ_2914.java
@@ -1,2 +1,14 @@
-package BOJ;public class BOJ_2914 {
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_2914 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int avg = input.nextInt();
+
+        int i = num*(avg-1) + 1;
+        System.out.println(i);
+    }
 }

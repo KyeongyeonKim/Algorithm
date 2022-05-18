34e321be7c7add486350a6b046aae9749adc132f
kyoung-yeon <randy43@naver.com>
2022-05-18 17:03:29 +0900

    BOJ_10869 / 사칙연산 / 사칙연산

---
+++ src/BOJ/BOJ_10869.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_10869 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}

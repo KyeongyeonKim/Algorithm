e15d7db56aa66bf73e55ab46cd443667f620768e
KY <rudyeon@naver.com>
2022-05-19 16:05:05 +0900

    BOJ_1550 / 16진수 / 구현

---
+++ src/BOJ/BOJ_1550.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1550 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String a = input.next();
+        System.out.println(Integer.parseInt(a,16));
+    }
+
+}

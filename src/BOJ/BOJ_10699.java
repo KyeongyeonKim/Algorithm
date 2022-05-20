87943a76a75e927f6be827cbcab31f329286a58f
KY <rudyeon@naver.com>
2022-05-20 16:48:55 +0900

    BOJ_10699 / 오늘 날짜 / 구현

---
+++ src/BOJ/BOJ_10699.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.time.LocalDate;
+
+public class BOJ_10699 {
+    public static void main(String[] args){
+        LocalDate Date = LocalDate.now();
+        System.out.println(Date);
+    }
+}
